74086f27f60919f35bf57c6396c91232fe6de198
Mihail Lovchitskiy <misha_aid@mail.ru>
2022-04-22 11:18:01 +0300

    Blin ball zhalko :(

--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -1,6 +1,6 @@
 #include "assignment/linked_list.hpp"
 
-#include <utility> // swap
+#include <utility>  // swap
 
 namespace assignment {
 
@@ -12,7 +12,18 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseIterative() {
-    // Напишите здесь свой код ...
+    Node* node = front_;
+    Node* prev = nullptr;
+    Node* next = nullptr;
+    for (int i = 0; i < size(); i++) {
+      next = node->next;
+      node->next = prev;
+      prev = node;
+      node = next;
+    }
+    node = front_;
+    front_ = back_;
+    back_ = node;
   }
 
   /**
@@ -23,12 +34,20 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseRecursive() {
-    // Напишите здесь свой код ...
+    if (size() > 0) {
+      reverse_recursive_helper(front_, nullptr);
+      Node* node = back_;
+      back_ = front_;
+      front_ = node;
+    }
   }
 
   // вспомогательный метод для реализации рекурсии
   void LinkedList::reverse_recursive_helper(Node*& curr, Node* prev) {
-    // Напишите здесь свой код ...
+    if (curr->next != nullptr) {
+      reverse_recursive_helper(curr->next, curr);
+    }
+    curr->next = prev;
   }
 
 }  // namespace assignment
